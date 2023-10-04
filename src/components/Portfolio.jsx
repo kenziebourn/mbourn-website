f3db732678f01b2ed6a6a3d0b89e0e9fd07e0d3d
kenziebourn <bournm@oregonstate.edu>
2023-10-04 13:53:25 -0700

    Updated projects + styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import jiaai from '../assets/portfolio/jiaai.png'
+import pearllabs from '../assets/portfolio/pearllabs.png'
 import mancala from '../assets/portfolio/mancala.png';
 import valet from '../assets/portfolio/valet.png';
 import exercisetrack from '../assets/portfolio/exercisetrack.png';
@@ -10,18 +12,29 @@ const Portfolio = () => {
     const portfolios = [
       {
         id: 1,
-        src: mancala,
-        href: 'https://github.com/kenziebourn/Mancala-Game',
+        src: jiaai,
+        href: 'https://github.com/kenziebourn/Song-Recommender',
       },
       {
         id: 2,
-        src: valet,
-        href: 'https://github.com/kenziebourn/ValetProgram',
+        src: pearllabs,
+        href: 'https://github.com/kenziebourn/PearlLabs',
       },
       {
         id: 3,
         src: exercisetrack,
-        href:'https://github.com/kenziebourn/Exercise-Tracker/tree/master'
+        href:'https://github.com/kenziebourn/Exercise-Tracker/tree/master',
+
+      },
+      {
+        id: 4,
+        src: valet,
+        href: 'https://github.com/kenziebourn/ValetProgram',
+      },
+      {
+        id: 5,
+        src: mancala,
+        href:'https://github.com/kenziebourn/Mancala-Game'
       },
     ];
   
@@ -42,16 +55,46 @@ const Portfolio = () => {
   
           <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
             {portfolios.map(({ id, src, href }) => (
-              <div key={id} className="shadow-md shadow-white rounded-lg">
+              <div key={id} className="rounded-lg outline: 2px solid bg-white">
                 <a href={href} target="_blank" rel="noopener noreferrer">
                     <img
                     href = {href}
                     src={src}
                     alt=""
-                    className="rounded-md duration-200 hover:scale-105"
+                    className="rounded-t hover:opacity-30 transition duration-300 ease-in-out"
             /* Alternative hover: make project image fade, changes opacity=> hover:opacity-30 transition duration-300 ease-in-out bg-green-800 */
                     />
                 </a>
+                { id === 1 && (
+                  <div className="text-center mt-2">
+                    <p className="text-black font-bold">Song Recommendation App</p>
+                    <p className="text-sm text-gray-500">#node.js  #mongoDB  #react.js </p>
+                  </div>
+                )}
+                { id === 2 && (
+                  <div className="text-center mt-2">
+                    <p className="text-black font-bold">Boba Shop Database</p>
+                    <p className="text-sm text-gray-500">#node.js #mySQL #html/css</p>
+                  </div>
+                )}
+                { id === 3 && (
+                  <div className="text-center mt-2">
+                    <p className="text-black font-bold">Exercise Tracker</p>
+                    <p className="text-sm text-gray-500">#node.js  #mongoDB  #react.js </p>
+                  </div>
+                )}
+                { id === 4 && (
+                  <div className="text-center mt-2">
+                    <p className="text-black font-bold">Valet Program</p>
+                    <p className="text-sm text-gray-500">#python</p>
+                  </div>
+                )}
+                { id === 5 && (
+                  <div className="text-center mt-2">
+                    <p className="text-black font-bold">Mancala Game</p>
+                    <p className="text-sm text-gray-500">#python</p>
+                  </div>
+                )}
               </div>
             ))}
           </div>
